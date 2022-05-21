8eb756a4a054b945cf9d5151d582175937b10da4
Aske Sønderby Knudsen <aske.soenderby.knudsen@ufst.dk>
2022-05-21 14:36:09 +0200

    buttons done

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function lessImportantVisible() {
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById("BtnDisinformation");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
