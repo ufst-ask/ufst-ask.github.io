027bc19ded4c1432a457c47deee42b61fb71ff8d
ufst-ask <aske.soenderby.knudsen@ufst.dk>
2022-06-10 12:35:28 +0200

    json, table, jQuery --> devLoad for offline json testing working

--- jquery_json_example/js/script.js
+++ jquery_json_example/js/script.js
@@ -1,6 +1,61 @@
+function devLoad() {
+	const txt = "[{\n" +
+		"\t\"name\": \"Disinformation\",\n" +
+		"\t\"tags\": [\"naming\", \"classes\", \"variables\", \"functions\"],\n" +
+		"\t\"status\": 0,\n" +
+		"\t\"workload\": 2,\n" +
+		"\t\"importance\": 3\n" +
+		"\t},\n" +
+		"\t{\n" +
+		"\t\t\"name\": \"Super duper naming\",\n" +
+		"\t\t\"tags\": [\"naming\", \"classes\", \"variables\", \"functions\"],\n" +
+		"\t\t\"status\": 0,\n" +
+		"\t\t\"workload\": 2,\n" +
+		"\t\t\"importance\": 3\n" +
+		"\t}\n" +
+		"]"
+	const resp = JSON.parse(txt);
+
+
+	var table = $('#table-list')
+	// Emptying the Table items
+	table.find('tbody').html('')
+
+	if (resp.length > 0) {
+		// If returned json data is not empty
+		var i = 1;
+		// looping the returned data
+		Object.keys(resp).map(k => {
+			// creating new table row element
+			var tr = $('<tr>')
+			// first column data
+			tr.append('<td class="py-1 px-2 text-center">' + (i++) + '</td>')
+			// second column data
+			tr.append('<td class="py-1 px-2">' + resp[k].name + '</td>')
+			// third column data
+			tr.append('<td class="py-1 px-2">' + resp[k].tags[0] + '</td>')
+			// fourth column data
+			tr.append('<td class="py-1 px-2">' + resp[k].status + '</td>')
+			// fifth column data
+			tr.append('<td class="py-1 px-2">' + resp[k].workload + '</td>')
+			// sixth column data
+			tr.append('<td class="py-1 px-2">' + resp[k].importance + '</td>')
+
+			// Append table row item to table body
+			table.find('tbody').append(tr)
+		})
+	} else {
+		// If returned json data is empty
+		var tr = $('<tr>')
+		tr.append('<th class="py-1 px-2 text-center">No data to display</th>')
+		table.find('tbody').append(tr)
+	}
+}
+
 function load_data() {
+	devLoad();
 	    // Show loader
-	    $('#loader').removeClass('d-none')
+	    /*$('#loader').removeClass('d-none')
 	        // Selecting the table Element
 	    var table = $('#table-list')
 	        // Emptying the Table items
@@ -31,13 +86,13 @@ function load_data() {
 	                            // second column data
 	                        tr.append('<td class="py-1 px-2">' + resp[k].name + '</td>')
 	                            // third column data
-	                        tr.append('<td class="py-1 px-2">' + resp[k].phone + '</td>')
+	                        tr.append('<td class="py-1 px-2">' + resp[k].tags[0] + '</td>')
 	                            // fourth column data
-	                        tr.append('<td class="py-1 px-2">' + resp[k].email + '</td>')
+	                        tr.append('<td class="py-1 px-2">' + resp[k].status + '</td>')
 	                            // fifth column data
-	                        tr.append('<td class="py-1 px-2">' + resp[k].region + '</td>')
+	                        tr.append('<td class="py-1 px-2">' + resp[k].workload + '</td>')
 	                            // sixth column data
-	                        tr.append('<td class="py-1 px-2">' + resp[k].country + '</td>')
+	                        tr.append('<td class="py-1 px-2">' + resp[k].importance + '</td>')
  
 	                        // Append table row item to table body
 	                        table.find('tbody').append(tr)
@@ -51,7 +106,7 @@ function load_data() {
 	                $('#loader').addClass('d-none')
 	            }
 	        })
-	    }, 500)
+	    }, 500)*/
 	}
  
 	$(function() {
